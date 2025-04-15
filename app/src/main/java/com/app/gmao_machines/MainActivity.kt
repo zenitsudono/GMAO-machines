a2a340c3b5eaf6388ec0491214d692417011eacf
amr._.dev <nasseramraoui2004@gmail.com>
2025-04-15 15:27:09 +0100

    Fixing splash not showing up error.

--- app/src/main/java/com/app/gmao_machines/MainActivity.kt
+++ app/src/main/java/com/app/gmao_machines/MainActivity.kt
@@ -3,6 +3,7 @@ package com.app.gmao_machines
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.app.gmao_machines.models.OnboardingPref
 import com.app.gmao_machines.navigation.AppNavigation
 import com.app.gmao_machines.ui.screens.OnboardingScreen
@@ -10,6 +11,13 @@ import com.app.gmao_machines.ui.theme.GMAOmachinesTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val splashScreen = installSplashScreen()
+
+        splashScreen.setOnExitAnimationListener { splashScreenViewProvider ->
+            splashScreenViewProvider.remove()
+        }
+
         super.onCreate(savedInstanceState)
         setContent {
             GMAOmachinesTheme {
